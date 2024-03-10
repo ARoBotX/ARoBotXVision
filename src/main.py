5dc9fcbe1091bbde59c7fad1b9a7770ce63beb4f
Tharindu Gimras <75577550+Hedes-Productions@users.noreply.github.com>
2024-03-10 10:44:57 +0530

    Add getting depth values

--- src/main.py
+++ src/main.py
@@ -5,6 +5,9 @@ from pykinect2.PyKinectV2 import *
 from pykinect2 import PyKinectRuntime
 from ultralytics import YOLO
 import torch
+import time
+import os
+
 
 def check_graphics_driver():
     print("Is CUDA supported by this system?",torch.cuda.is_available())
@@ -23,6 +26,8 @@ class KinectHandler(object):
         self.depth_width, self.depth_height = self.kinect_depth_sensor.depth_frame_desc.Width, self.kinect_depth_sensor.depth_frame_desc.Height
         self.kinect_color_sensor = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Color)
         self.color_width, self.color_height = self.kinect_color_sensor.color_frame_desc.Width, self.kinect_color_sensor.color_frame_desc.Height
+        self.color_frames_buffer = []
+        self.depth_frames_buffer = []
 
     def get_depth_frame(self):
         if self.kinect_depth_sensor.has_new_depth_frame():
@@ -45,47 +50,67 @@ class KinectHandler(object):
         self.kinect_color_sensor.close()
 
 
+def get_depth_frame_with_timestamp(kinect):
+    depth_frame = kinect.get_depth_frame()
+    return depth_frame, time.time()
 
+    
+def get_color_frame_with_timestamp(kinect):
+    color_frame = kinect.get_color_frame()
+    return color_frame, time.time()
 
+    
 def main():
+    time_lag = 0.01
     check_graphics_driver()
     model_path = './src/model/last.pt'
     
     kinect = KinectHandler()
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f'Using device: {device}')
+    model = YOLO(model_path).to(device)
 
     while True:
-        # # Capture depth frame
-        # depth_frame = kinect.get_depth_frame()
 
-        # #print(depth_frame)
-        # if depth_frame is not None:
-        #     # Display depth frame
-        #     print(depth_frame)
-        #     cv2.imshow('Depth Frame', depth_frame.astype(np.uint8))
-
-        color_frame = kinect.get_color_frame()
-
+        depth_frame, depth_timestamp = get_depth_frame_with_timestamp(kinect) # (424, 512)
+        color_frame, color_timestamp = get_color_frame_with_timestamp(kinect) # (1080, 1920, 4)
+        if depth_frame is not None:
+            kinect.depth_frames_buffer.append([depth_frame, depth_timestamp])
         if color_frame is not None:
-            cv2.imwrite('input.png', color_frame)
-            #cv2.imshow('Color Frame', color_frame.astype(np.uint8))
-            img = cv2.imread('input.png')
-            H, W , _ = img.shape
-            device = 'cuda' if torch.cuda.is_available() else 'cpu'
-            print(f'Using device: {device}')
-            model = YOLO(model_path).to(device)
+            kinect.color_frames_buffer.append([color_frame, color_timestamp])
+        # print(kinect.depth_frames_buffer[-1][0])
+        if len(kinect.depth_frames_buffer)>0 and len(kinect.color_frames_buffer)>0:
+            if kinect.depth_frames_buffer[-1][0] is not None and kinect.color_frames_buffer[-1][0] is not None:
+                color_frame, color_timestamp = kinect.color_frames_buffer[-1]
+                depth_frame, depth_timestamp = kinect.depth_frames_buffer[-1]
+                timestamp_difference = abs(color_timestamp-depth_timestamp)
+                if timestamp_difference<=time_lag:
+                    color_frame_RGB = color_frame[:, :, :3]   # Use color_frame directly
 
-            results = model(img)
-            for result in results:
-                if result.masks is not None:
-                    print("Identified a box")
-                    for j, mask in enumerate(result.masks.data):
-                        mask = mask.numpy() * 255
-                        mask = cv2.resize(mask, (W, H))
-                        cv2.imwrite('output.png', mask)
-                        np.savetxt(f'output.csv', mask, delimiter=',', fmt='%d')
+                    H, W  = depth_frame.shape #(1080 1920 3)
 
-        
 
+                    results = model(color_frame_RGB, verbose=False)
+                    for result in results:
+                        if result.masks is not None:
+                            print("Identified a box")
+                            for j, mask in enumerate(result.masks.data):
+                                mask = mask.to(device)
+                                mask = mask.cpu().numpy() * 255 if device == 'cuda' else mask.numpy() * 255
+                                                        
+                                mask = cv2.resize(mask, (W, H)).astype(np.uint8)
+                                
+                                depth_frame_with_mask = cv2.bitwise_and(depth_frame, depth_frame, mask=mask)
+                                distance_to_object = np.mean(depth_frame_with_mask)
+                                # my_path = 'D:/tricalseventhsem/ARoBotXVision/src/outputs'
+                                # cv2.imwrite(os.path.join(my_path, 'depth_with_mask.jpg'), depth_frame_with_mask)
+                                # cv2.imwrite(os.path.join(my_path, 'depth_frame.jpg'), depth_frame_with_mask)
+                                # cv2.imwrite(os.path.join(my_path, 'color_frame.jpg'), color_frame_RGB)
+                                # cv2.imwrite(os.path.join(my_path, 'mask.jpg'), mask)
+                                # cv2.imshow('Depth Frame with Mask', depth_frame_with_mask)
+
+            
+            
         # Press 'q' to quit
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
