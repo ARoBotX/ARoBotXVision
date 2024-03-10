43464506ba881bfb03bc99813bbe0ceb6c5b4c74
dev-pa5an <pasanfernando24@gmail.com>
2024-03-10 18:00:24 +0530

    initial commit

--- src/main.py
+++ src/main.py
@@ -4,87 +4,83 @@ from pykinect2 import PyKinectV2
 from pykinect2.PyKinectV2 import *
 from pykinect2 import PyKinectRuntime
 from ultralytics import YOLO
+import time
+import paho.mqtt.client as mqtt
+import ssl
+import json
+import _thread
 
+def on_connect(client, userdata, flags, rc):
+    print("Connected to AWS IoT: " + str(rc))
 
 class KinectHandler(object):
     def __init__(self):
-        self.kinect_depth_sensor = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Depth)
-        self.depth_width, self.depth_height = self.kinect_depth_sensor.depth_frame_desc.Width, self.kinect_depth_sensor.depth_frame_desc.Height
-        self.kinect_color_sensor = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Color)
-        self.color_width, self.color_height = self.kinect_color_sensor.color_frame_desc.Width, self.kinect_color_sensor.color_frame_desc.Height
+        self.kinectd = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Depth)
+        self.depth_width, self.depth_height = self.kinectd.depth_frame_desc.Width, self.kinectd.depth_frame_desc.Height
+        self.kinect = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Color)
+        self.color_width, self.color_height = self.kinect.color_frame_desc.Width, self.kinect.color_frame_desc.Height
 
     def get_depth_frame(self):
-        if self.kinect_depth_sensor.has_new_depth_frame():
-            depth_frame = self.kinect_depth_sensor.get_last_depth_frame()
+        if self.kinectd.has_new_depth_frame():
+            depth_frame = self.kinectd.get_last_depth_frame()
             depth_frame = depth_frame.reshape((self.depth_height, self.depth_width)).astype(np.uint16)
             return depth_frame
         return None
 
     def get_color_frame(self):
-        if self.kinect_color_sensor.has_new_color_frame():
-            color_frame = self.kinect_color_sensor.get_last_color_frame()
+        if self.kinect.has_new_color_frame():
+            color_frame = self.kinect.get_last_color_frame()
             color_frame = color_frame.reshape((self.color_height, self.color_width, 4))
+            color_frame = color_frame[:, :, :3]
             return color_frame
         return None
 
-    def close_depth_sensor(self):
-        self.kinect_depth_sensor.close()
-
-    def close_color_sensor(self):
-        self.kinect_color_sensor.close()
-
-
-
+    def close(self):
+        self.kinect.close()
 
 def main():
     model_path = 'last.pt'
+    model = YOLO(model_path)
 
     kinect = KinectHandler()
 
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.tls_set(ca_certs='./rootCA.pem', certfile='./certificate.pem.crt', keyfile='./private.pem.key', tls_version=ssl.PROTOCOL_SSLv23)
+    client.tls_insecure_set(True)
+    client.connect("a1cm3c34iajtv7-ats.iot.us-east-1.amazonaws.com", 8883, 60)
+
     while True:
-        # # Capture depth frame
-        # depth_frame = kinect.get_depth_frame()
+        depth_frame = kinect.get_depth_frame()
 
-        # #print(depth_frame)
-        # if depth_frame is not None:
-        #     # Display depth frame
-        #     print(depth_frame)
-        #     cv2.imshow('Depth Frame', depth_frame.astype(np.uint8))
+        if depth_frame is not None:
+            cv2.imshow('Depth Frame', depth_frame.astype(np.uint8))
 
-        color_frame = kinect.get_color_frame()
+            color_frame = kinect.get_color_frame()
 
-        if color_frame is not None:
-            cv2.imwrite('input.png', color_frame)
-            #cv2.imshow('Color Frame', color_frame.astype(np.uint8))
-            img = cv2.imread('input.png')
-            H, W , _ = img.shape
+            if color_frame is not None:
+                H, W , _ = color_frame.shape
 
-            model = YOLO(model_path)
+                results = model(color_frame, verbose=False)
 
-            results = model(img, verbose=False)
-            for result in results:
+                for result in results:
+                    if result.masks is not None:
+                        print("Identified a box")
+                        for mask in result.masks.data:
+                            mask = mask.numpy().astype(np.uint8) * 255
+                            mask = cv2.resize(mask, (W, H))
+                            mask_resized = cv2.resize(mask, (kinect.depth_width, kinect.depth_height))
+                            mask_depth_values = depth_frame[mask_resized > 0]
+                            if mask_depth_values.size > 0:
+                                average_depth = np.mean(mask_depth_values)
+                                print("Average depth of pixels in mask:", average_depth)
+                                client.publish("wheelChair/depth", payload=json.dumps({"msg": average_depth}), qos=0, retain=False)
 
-                if result.masks is not None:
-                    print("Identified a box")
-                    break
-                #     for j, mask in enumerate(result.masks.data):
-                #         mask = mask.numpy() * 255
-                #         mask = cv2.resize(mask, (W, H))
-                #         cv2.imwrite('output.png', mask)
-
-
-
-
-        # Press 'q' to quit
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    # Release the Kinect resources
-    kinect.close_color_sensor()
+    kinect.close()
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
-
-
-
